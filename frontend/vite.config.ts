d28e820d23aec7ad90e58cd9954367dae311bcf5
Ericwimsatt <ericwimsatt@gmail.com>
2024-12-01 11:43:38 -0800

    change vite config to try to deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  // build: {
-  //   outDir: path.resolve(__dirname, '../dist')
-  // }
+  base: '/gingernet/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
